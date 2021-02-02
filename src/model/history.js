12b891db3c3dff7eea27d1aab8d551e199d5b6f6
amardito <amardito37@gmail.com>
2021-02-02 18:33:24 +0700

    changes model topup using id virtual

--- src/model/history.js
+++ src/model/history.js
@@ -89,32 +89,37 @@ const subscriptionModel = (payload) =>  new Promise((resolve, reject) => {
 })
 
 const topupModel = (payload) =>  new Promise((resolve, reject) => {
-  const Qstr = 'INSERT INTO HistoryOther SET ?'
-  const id = Date.now()
+  const id = Date.now() 
 
-  const payloads = {
-    id_history : `sub-${id}`,
-    ...payload,
-    type: 'Top up',
-    createdAt: new Date(Date.now()),
-  }
-  db.query(Qstr, payloads, (err) => {
+  const selectTransfer = `SELECT id_user, balance FROM users WHERE id_virtual LIKE '%${payload.id_virtual}%'`
+  db.query(selectTransfer, (err,data) => {
     if (err) {
       reject(err)
     }
-    const selectTransfer = `SELECT balance FROM users WHERE id_user = ${payload.id_user}`
-    db.query(selectTransfer, (err,data) => {
+
+    const Qstr = 'INSERT INTO HistoryOther SET ?'
+    const payloads = {
+      id_history : `top-${id}`,
+      id_user : data[0].id_user,
+      id_instance : payload.id_instance,
+      balance : payload.balance,
+      type: 'Top up',
+      createdAt: new Date(Date.now()),
+    }
+    db.query(Qstr, payloads, (err) => {
       if (err) {
         reject(err)
       }
-      const updateTransfer = `UPDATE users SET ? WHERE id_user = ${payload.id_user}`
-      db.query(updateTransfer, {balance: data[0].balance + payload.balance}, (err) => {
-        if (err) {
-          reject(err)
-        }
-      })
     })
-  })
+
+    const updateTransfer = `UPDATE users SET ? WHERE id_user = ${data[0].id_user}`
+    db.query(updateTransfer, {balance: data[0].balance + payload.balance}, (err) => {
+      if (err) {
+        reject(err)
+      }
+    })
+
+  })  
 
   resolve(payload)
 
