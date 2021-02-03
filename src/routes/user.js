218d55eee1ac93587c9c086d78522114394b9d97
Moch. Bayu Erich Dewangga <70324722+bayue48@users.noreply.github.com>
2021-02-04 01:42:14 +0700

    handle same phone number

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,10 +6,10 @@ const verif = require("../middlewares/verif");
 
 userRouter.get("/:id", userController.getUserById);
 userRouter.get("/phone/:phone", userController.getUserPhone);
-userRouter.patch("/:id", userController.updateUser);
+userRouter.patch("/:id", verif.phoneUsed, userController.updateUser);
 userRouter.patch("/pw/new", verif.isLogin, userController.updatePass);
 userRouter.patch("/img/:id", uploadImg.singleUpload, userController.updateUser);
-userRouter.patch("/PIN", verif.isLogin, userController.SetPIN);
+userRouter.patch("/PIN/new", verif.isLogin, userController.SetPIN);
 userRouter.get("/PIN/:PIN", verif.isLogin, userController.CheckPIN);
 
 module.exports = userRouter;
